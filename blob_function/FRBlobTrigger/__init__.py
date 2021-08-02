5ee1de2611afe94dcc159d675dd402a3d4a2ae39
MixKlim <mixklim@gmail.com>
2021-08-03 00:16:42 +0200

    added azure blob function and real-time data processing

--- blob_function/FRBlobTrigger/__init__.py
+++ blob_function/FRBlobTrigger/__init__.py
@@ -1,9 +1,22 @@
 import logging
-
+import io
+import numpy as np
 import azure.functions as func
+import pandas as pd
+import datetime
 
 
-def main(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
+def main(inputBlob: func.InputStream, predictions: func.Out[str]):
+
+    df = pd.read_csv(io.BytesIO(inputBlob.read()), sep=',')
+    # logging.info(f"data loaded: {df}")
+
+    # Baseline model: Shifted by 15 minutes
+    df_load_rt = df.copy(deep=True).reset_index()[["load_actuals_mw"]]
+    df_load_rt["data_index_"] = pd.to_datetime(df.reset_index()["data_index_"]) + datetime.timedelta(minutes=15)
+    df_load_rt = df_load_rt.set_index("data_index_").rename(columns={"load_actuals_mw": "predictions"})
+    prediction = df_load_rt['predictions'].to_numpy()
+
+    # logging.info(f"prediction: {prediction}")
+    result = np.array_str(prediction)
+    predictions.set(result)
