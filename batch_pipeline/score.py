47a1685cbfefebad1875a29d50aaebd74cc1024d
MixKlim <mixklim@gmail.com>
2021-08-07 11:22:31 +0200

    added weekly batch inference, batch dashboard, presentation

--- batch_pipeline/score.py
+++ batch_pipeline/score.py
@@ -19,9 +19,13 @@ def run(mini_batch):
     # process each file in the batch
     for f in mini_batch:
         # Read comma-delimited data into an array
-        data = np.genfromtxt(f, delimiter=',')
+        data = np.genfromtxt(f, delimiter=',', skip_header=1)
         # Reshape into a 2-dimensional array for model input
-        prediction = model.predict(data.reshape(1, -1))
+        data = data[:, 1:]
+        prediction = model.predict(data)
+        # log results (for application insights)
+        log = 'Data:' + str(data) + ' - Prediction:' + str(prediction)
+        print(log)
         # Append prediction to results
-        resultList.append("{}: {}".format(os.path.basename(f), prediction[0]))
+        resultList.append("{}: {}".format(os.path.basename(f), np.mean(prediction)))
     return resultList
